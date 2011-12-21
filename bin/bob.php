56cd10ca1f768b5322ad640418c5328a96a6ae06
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2011-12-21 16:50:54 +0100

    Added some comments

--- bin/bob.php
+++ bin/bob.php
@@ -9,12 +9,15 @@ class Bob
     static $application;
 }
 
+// This is the top-level application instance, which holds all
+// tasks and contains the logic for running them.
 Bob::$application = new Application;
 
 try {
     Bob::$application->run();
 
 } catch (\Exception $e) {
+    // Print exceptions to STDERR and exit with an error.
     println($e, STDERR);
     exit(1);
 }
