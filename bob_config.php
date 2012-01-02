d209d4308405a21bda12e3fa772d503ebef7df7b
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-01-02 20:01:04 +0100

    Removed example tasks

--- bob_config.php
+++ bob_config.php
@@ -16,15 +16,15 @@ $pharFiles = FileList(array(
     'vendor/Getopt.php',
 ));
 
+// The "default" task is invoked when there's no
+// task explicitly given on the command line.
 task('default', array('dist'));
 
 // Note: All file paths used here should be relative to the project
 // directory. Bob automatically sets the current working directory
 // to the path where the `bob_config.php` resides.
 
-// The first defined task is the default task for the case
-// Bob is executed without a task name.
-desc('Makes a distributable version of Bob, consisting of a composer.json 
+desc('Makes a distributable version of Bob, consisting of a composer.json
       and a PHAR file.');
 task('dist', array('composer.json', 'bin/bob.phar'));
 
@@ -45,11 +45,13 @@ require 'phar://bob.phar/bin/bob.php';
 __HALT_COMPILER();
 EOF;
 
+    $projectDir = Bob::$application->projectDir;
+
     $phar = new \Phar($task->name, 0, basename($task->name));
     $phar->startBuffering();
 
     foreach ($task->prerequisites as $file) {
-        $phar->addFile($file, substr($file, (strpos(getcwd(), $file) === 0) ? (strlen(getcwd()) + 1) : 0));
+        $phar->addFile($file, substr($file, (strpos($projectDir, $file) === 0) ? (strlen($projectDir) + 1) : 0));
     }
 
     $phar->setStub($stub);
@@ -75,7 +77,6 @@ desc('Takes an environment variable PREFIX and writes a `bob` executable
 task('install', array('bin/bob.phar'), function($task) {
     $prefix = getenv('PREFIX') ?: '/usr/local';
 
-
     $success = copy('bin/bob.phar', "$prefix/bin/bob");
     chmod("$prefix/bin/bob", 0755);
 
@@ -94,34 +95,5 @@ task('uninstall', function($task) {
     if (false !== unlink("$prefix/bin/bob")) {
         println("Erased bob successfully from $prefix");
     }
-});
-
-/*
- * Each Task consists of an optional `desc()` call
- * with a meaningful Task description, and a call to
- * `task()` with the task name and task body (usually
- * as Closure).
- *
- * **Important:** The call to `desc()` must be _before_
- * the call to `task()`.
- *
- * The only argument a task receives is the task instance.
- * Via the task instance you've access to the prerequisites
- * and name of the task which is very useful for file tasks.
- */
-desc('Example: Says "Hello World NAME!"', 'greet NAME');
-task('greet', array('foo'), function($task) {
-    echo "Hello World! I'm the {$task->name} Task!\n";
-
-    echo "I've following prerequisites:\n";
-    echo " - ", join("\n - ", $task->prerequisites), "\n";
-});
-
-task('foo', array('bar'), function() {
-    echo "This is the Foo Task\n";
-});
-
-task('bar', function() {
-    echo "This is the Bar Task\n";
 });
 
