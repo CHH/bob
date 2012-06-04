193202ce9f4ac6a1628eb3451dc028684ff3bd18
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-06-04 21:02:09 +0200

    Moved to autoloading.

--- bin/bob.php
+++ bin/bob.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__.'/../lib/bob.php';
+require __DIR__.'/../lib/Bob.php';
 
 // This is the top-level application instance, which holds all
 // tasks and contains the logic for running them.
---
+++ lib/Bob.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(__DIR__ . "/../vendor/autoload.php");
+require_once(__DIR__ . "/Bob/Dsl.php");
+
+# Internal: Holds the current application instance.
+class Bob
+{
+    const VERSION = "v0.1.0";
+
+    # Instance of \Bob\Application
+    static $application;
+}
