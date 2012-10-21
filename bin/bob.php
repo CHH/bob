3df3d8c20f842a2f015fcba93da1aae13e31153b
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-10-21 09:35:13 +0200

    Use right path to vendor/autoload.php

--- bin/bob.php
+++ bin/bob.php
@@ -1,6 +1,19 @@
 <?php
 
-require __DIR__.'/../lib/Bob.php';
+function includeIfExists($file)
+{
+    if (file_exists($file)) {
+        return $file;
+    }
+}
+
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL);
+}
+
+require_once(__DIR__ . '/../lib/Bob.php');
 
 // This is the top-level application instance, which holds all
 // tasks and contains the logic for running them.
