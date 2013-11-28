cfadba4414b1106ec7c130d6c172a4fcd466b8ea
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2013-11-28 19:39:57 +0100

    Add library with Composer tasks

---
+++ lib/Bob/Library/ComposerLibrary.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Bob\Library;
+
+use Bob\Application;
+use Bob\TaskLibraryInterface;
+use Bob\BuildConfig as b;
+
+class ComposerLibrary implements TaskLibraryInterface
+{
+    function register(Application $app)
+    {
+    }
+
+    function boot(Application $app)
+    {
+        $app->task('composer.phar', function($task) {
+            if (file_exists($task->name)) {
+                return true;
+            }
+
+            $src = fopen('http://getcomposer.org/composer.phar', 'rb');
+            $dest = fopen($task->name, 'wb');
+
+            stream_copy_to_stream($src, $dest);
+        });
+
+        $app->task('composer:install', array('composer.phar'), function() {
+            b\php(array('composer.phar', 'install'));
+        });
+
+        $app->task('composer:update', array('composer.phar'), function() {
+            b\php(array('composer.phar', 'update'));
+        });
+
+        $app->fileTask('composer.lock', array('composer.phar', 'composer.json'), function($task) use ($app) {
+            $app->task('composer:update')->invoke();
+        });
+    }
+}
